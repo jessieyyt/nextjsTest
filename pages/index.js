a40af0f8759047785399189670ada1d8cafb647d
jessieyyt <131130380+jessieyyt@users.noreply.github.com>
2026-04-15 04:24:24 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,3 @@ export async function getServerSideProps() {
 export default function Home({ time }) {
     return <div><h1>Home Page</h1><p>Rendered at: {time}</p></div>;
 }
-pages/about.js:
-
-export async function getServerSideProps() {
-    return { props: { time: new Date().toISOString() } };
-}
-
-export default function About({ time }) {
-    return <div><h1>About Page</h1><p>Rendered at: {time}</p></div>;
-}
